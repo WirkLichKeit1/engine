a5c959fc95040727b71c1178ba09c07fee6d5c90
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2026-05-13 15:43:03 +0000

    up

--- engine/biomes/BiomesMap.ts
+++ engine/biomes/BiomesMap.ts
@@ -3,7 +3,8 @@ import { WORLD } from "../World"
 export type BiomeType = "prairie" | "forest" | "desert" | "swamp"
 
 export interface Biome {
-    type: BiomeType               // multiplicador de chance de spawn de comida
+    type: BiomeType
+    foodSpawnMultiplier: number   // multiplicador de chance de spawn de comida
     energyDrainMultiplier: number // multiplicador de custo de energia
     speedMultiplier: number       // multiplicador de velocidade
     color: number                 // cor PIXI para o frontend
